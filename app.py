d6b230e2ce47cff765a41dffc837b172078f1075
Jeferson.s <jeferson.eng23@gmail.com>
2025-05-08 18:20:08 -0300

    Desligar cash para atualizar automaticamente no navegador da TV

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import json, os
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -8,6 +9,13 @@ LINKS_FILE = os.path.join(os.path.dirname(__file__), 'links.json')
 def carregar_links():
     with open(LINKS_FILE, 'r') as f:
         return json.load(f)
+    
+@app.after_request
+def add_header(response):
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
 
 @app.route('/')
 def index():
@@ -23,7 +31,8 @@ def relatorios():
         return redirect(url_for('index'))
 
     links = dados
-    return render_template('relatorios.html', setor=setor, links_json=json.dumps(links))  # JSON como string segura
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    return render_template('relatorios.html', setor=setor, links_json=json.dumps(links), timestamp=timestamp)  # JSON como string segura
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8050)
